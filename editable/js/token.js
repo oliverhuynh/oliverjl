6a8dd9986e37407853794d48b5f29e325fd0bee5
Oliver Huynh <oliver@jufist.com>
2018-09-24 17:38:46 +0700

    Token fixes

--- editable/js/token.js
+++ editable/js/token.js
@@ -5,10 +5,10 @@
 
   (function($) {
     Joomla.livetoken = {
-      findOldtoken: function() {
+      findOldtoken: function($form) {
         var old;
         old = false;
-        $("input[name][type=hidden]").each(function() {
+        $form.find("input[name][type=hidden]").each(function() {
           if ($(this).attr("name").length === 32) {
             old = $(this).attr("name");
             return false;
@@ -16,21 +16,57 @@
         });
         return old;
       },
-      attach: function() {
-        var oldToken;
-        oldToken = this.findOldtoken();
-        if (!oldToken) {
-          return;
+      callbacks: [],
+      findNewToken: function(callback) {
+        var t = this;
+        // Only request one time;
+        if (t.callbacks.length >= 1) {
+          this.callbacks.push(callback);
+          t.processCallbacks();
+          return ;
         }
+        this.callbacks.push(callback);
         $.getJSON("/libraries/oliverjl/editable/js/token.php", function(data) {
-          var newToken;
-          newToken = data.token || "na";
-          if (Joomla.optionsStorage) {
-            Joomla.optionsStorage["csrf.token"] = newToken;
-          }
-          $("input[name='" + oldToken + "']").attr("name", newToken);
-          console.warn(["got token", data]);
+          t.data = data;
+          t.processCallbacks();
         });
+      },
+      processCallbacks: function() {
+        var t = this
+        if (!t.data) {
+          return ;
+        }
+        $.each(this.callbacks, function() {
+          this(t.data);
+        });
+      },
+      disableForm: function() {
+        var t = this;
+        $("form").filter(':not(.livetoken)').addClass('livetoken').submit(function(e){
+            if (!$(this).hasClass('livetoken-added')) {
+              e.preventDefault();
+            }
+        }).each(function() {
+          var oldToken;
+          var $form = $(this);
+          oldToken = t.findOldtoken($(this));
+          if (!oldToken) {
+            $(this).addClass('livetoken-added livetoken-notoken');
+            return;
+          }
+          t.findNewToken(function(data) {
+            var newToken;
+            newToken = data.token || "na";
+            if (Joomla.optionsStorage) {
+              Joomla.optionsStorage["csrf.token"] = newToken;
+            }
+            $form.find("input[name='" + oldToken + "']").attr("name", newToken);
+            $form.addClass('livetoken-added');
+          });
+        });
+      },
+      attach: function() {
+        this.disableForm();
       }
     };
     Joomla.livetoken.attach();
